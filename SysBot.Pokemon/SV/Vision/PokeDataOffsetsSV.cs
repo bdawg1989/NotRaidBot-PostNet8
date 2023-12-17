2bdf189e1f955bb9d70307ea74ec1606c3eb55ee
Gengar <hhbrandon.stone@gmail.com>
2023-12-16 18:44:14 -0600

    3.0.0 Update

--- SysBot.Pokemon/SV/Vision/PokeDataOffsetsSV.cs
+++ SysBot.Pokemon/SV/Vision/PokeDataOffsetsSV.cs
@@ -10,14 +10,14 @@ namespace SysBot.Pokemon
         public const string SVGameVersion = "3.0.0";
         public const string ScarletID = "0100A3D008C5C000";
         public const string VioletID = "01008F6008C5E000";
-        public IReadOnlyList<long> BoxStartPokemonPointer { get; } = new long[] { 0x47350D8, 0x1C0, 0x30, 0x9D0, 0x0 };// 3.0.0
-        public IReadOnlyList<long> MyStatusPointer { get; } = new long[] { 0x47350D8, 0x1C0, 0x0, 0x40 }; // KMyStatus - TeraFinder or Official 3.0.0
-        public IReadOnlyList<long> ConfigPointer { get; } = new long[] { 0x47350D8, 0x1C0, 0xD0, 0x40 };// 3.0.0
+        public IReadOnlyList<long> BoxStartPokemonPointer { get; } = new long[] { 0x47350D8, 0xD8, 0x8, 0xB8, 0x30, 0x9D0, 0x0 };// 3.0.0
+        public IReadOnlyList<long> MyStatusPointer { get; } = new long[] { 0x47350D8, 0xD8, 0x8, 0xB8, 0x0, 0x40 }; // KMyStatus - TeraFinder or Official 3.0.0
+        public IReadOnlyList<long> ConfigPointer { get; } = new long[] { 0x47350D8, 0xD8, 0x8, 0xB8, 0xD0, 0x40 };// 3.0.0
         public IReadOnlyList<long> CurrentBoxPointer { get; } = new long[] { 0x47350D8, 0x1C0, 0x28, 0x570 };// 3.0.0
         public IReadOnlyList<long> LinkTradePartnerNIDPointer { get; } = new long[] { 0x475EA28, 0xF8, 0x8 };// 3.0.0
         public IReadOnlyList<long> Trader2MyStatusPointer { get; } = new long[] { 0x473A110, 0x48, 0xE0, 0x0 }; // 3.0.0
-        public IReadOnlyList<long> IsConnectedPointer { get; } = new long[] { 0x46F0EE8, 0x68 }; // 3.0.0
-        public IReadOnlyList<long> OverworldPointer { get; } = new long[] { 0x473ade0, 0x160, 0xE8, 0x28 }; // 3.0.0
+        public IReadOnlyList<long> IsConnectedPointer { get; } = new long[] { 0x4763E08, 0x10 }; // 3.0.0
+        public IReadOnlyList<long> OverworldPointer { get; } = new long[] { 0x473ADE0, 0x160, 0xE8, 0x28 }; // 3.0.0
         // RaidCrawler Offsets
         public IReadOnlyList<long> BlockKeyPointer { get; } = new long[] { 0x47350D8, 0xD8, 0x0, 0x0, 0x30, 0x0 }; // RaidCrawler Offsets.cs // 3.0.0
         public IReadOnlyList<long> RaidBlockPointerP { get; } = new long[] { 0x47350D8, 0x1C0, 0x88, 0x40 }; // RaidBlockPointerBase RaidCrawler (Can use same as ConfigPointer) 3.0.0
