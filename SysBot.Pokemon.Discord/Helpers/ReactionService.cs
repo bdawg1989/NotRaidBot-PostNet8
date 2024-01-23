ebb21dc987cc811f9c729f26332a058c9cb6a0cd
Gengar <hhbrandon.stone@gmail.com>
2024-01-23 17:00:59 -0600

    Add timer and remove handler to prevent memory leaks/usage

--- SysBot.Pokemon.Discord/Helpers/ReactionService.cs
+++ SysBot.Pokemon.Discord/Helpers/ReactionService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Timers;
 using Discord;
 using Discord.WebSocket;
 
@@ -8,24 +9,43 @@ public class ReactionService
 {
     private readonly DiscordSocketClient _client;
     private readonly Dictionary<ulong, Func<SocketReaction, Task>> _reactionActions;
+    private readonly Dictionary<ulong, Timer> _reactionTimers;
 
     public ReactionService(DiscordSocketClient client)
     {
         _client = client;
         _reactionActions = new Dictionary<ulong, Func<SocketReaction, Task>>();
+        _reactionTimers = new Dictionary<ulong, Timer>();
 
-        // Subscribe to the reaction added event
         _client.ReactionAdded += OnReactionAddedAsync;
     }
 
     public void AddReactionHandler(ulong messageId, Func<SocketReaction, Task> handler)
     {
         _reactionActions[messageId] = handler;
+
+        var timer = new Timer(600000); // 600000 milliseconds = 10 minutes
+        timer.Elapsed += (sender, args) => RemoveReactionHandler(messageId);
+        timer.AutoReset = false; // Trigger only once
+        timer.Start();
+
+        _reactionTimers[messageId] = timer;
     }
 
     public void RemoveReactionHandler(ulong messageId)
     {
-        _reactionActions.Remove(messageId);
+        if (_reactionActions.ContainsKey(messageId))
+        {
+            _reactionActions.Remove(messageId);
+        }
+
+        // Stop and dispose of the timer if it exists
+        if (_reactionTimers.TryGetValue(messageId, out var timer))
+        {
+            timer.Stop();
+            timer.Dispose();
+            _reactionTimers.Remove(messageId);
+        }
     }
 
     private async Task OnReactionAddedAsync(Cacheable<IUserMessage, ulong> cachedMessage, Cacheable<IMessageChannel, ulong> cachedChannel, SocketReaction reaction)
@@ -33,6 +53,7 @@ public class ReactionService
         if (_reactionActions.TryGetValue(reaction.MessageId, out var handler))
         {
             await handler(reaction);
+            RemoveReactionHandler(reaction.MessageId); // Remove handler after reaction
         }
     }
 }
